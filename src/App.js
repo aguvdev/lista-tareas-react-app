a627a1199df73144ed8a5f8c8c8a38bc1f29a6af
hannagustinvega <hannheitiragustin@hotmail.com>
2022-06-06 17:59:55 -0300

    update: estructura del contenedor lista para componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Logo from './components/Logo';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hola amigue</p>
+    <div className='aplicacion-tareas-container'>
+      <Logo 
+          titulo={'Lista de Tareas'} />
+      <div className='lista-tareas-principal'>
+        <h2>Mis Tareas</h2>
+      </div>
     </div>
   );
 }
