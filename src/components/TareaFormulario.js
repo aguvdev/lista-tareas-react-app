5b04ebfdb8f8cbe05c63a728b078d3062f529190
hannagustinvega <hannheitiragustin@hotmail.com>
2022-06-07 21:19:43 -0300

    update: componente TareaFormulario completado

--- src/components/TareaFormulario.js
+++ src/components/TareaFormulario.js
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
 import '../stylesheets/TareaFormulario.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function TareaFormulario(props) {
+
+  const [input, setInput] = useState('');
+
+  const manejarCambio = e => {
+    setInput(e.target.value);
+  };
+
+  const manejarEnvio = e => {
+    e.preventDefault();
+
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completado: false
+    }
+
+    props.onSubmit(tareaNueva);
+  };
+
+
   return (
-    <form className='tareaFormulario-container'>
+    <form 
+        className='tarea-formulario-container'
+        onSubmit={manejarEnvio}>
       <input 
           className='tarea-input'
           type='text'
           placeholder='Escribe una Tarea'
           name='texto'
+          onChange={manejarCambio}
       />
       <button className='tarea-button'>
         Agregar Tarea
